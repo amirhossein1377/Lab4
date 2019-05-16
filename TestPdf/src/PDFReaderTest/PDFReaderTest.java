4d70a82d38e5f2924f4f83c96ea95877e798b768
amirhossein1377 <50528985+amirhossein1377@users.noreply.github.com>
2019-05-16 14:06:27 +0430

    Project Lab 4 uploaded

---
+++ TestPdf/src/PDFReaderTest/PDFReaderTest.java
@@ -0,0 +1,20 @@
+package PDFReaderTest;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+
+public class PDFReaderTest {
+    public static void main(String[] args) {
+        try {
+            PdfReader pdfReader = new PdfReader("C:\\Users\\amirhossein\\Desktop\\test.pdf");
+            int pages = pdfReader.getNumberOfPages();
+            for (int i=1; i<=pages; i++) {
+                String pageContent = PdfTextExtractor.getTextFromPage(pdfReader, i);
+                System.out.println("Content on Page " + i + ": " + pageContent);
+            }
+        }   catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
